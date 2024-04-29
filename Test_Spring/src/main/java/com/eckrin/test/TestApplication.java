384b784a53a548fbab13287927262cabeec3c87c
eckrin <bsh51515@naver.com>
2024-04-29 23:39:22 +0900

    feat: 스케줄러를 활용한 Spring Batch 구동
    - batch.job.enabled: false 설정
    - @EnableScheduling 어노테이션 설정

--- Test_Spring/src/main/java/com/eckrin/test/TestApplication.java
+++ Test_Spring/src/main/java/com/eckrin/test/TestApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 //@EnableBatchProcessing
 @SpringBootApplication
+@EnableScheduling
 public class TestApplication {
 
 	public static void main(String[] args) {
---
+++ Test_Spring/src/main/java/com/eckrin/test/spring_batch/scheduler/SampleScheduler.java
@@ -0,0 +1,36 @@
+package com.eckrin.test.spring_batch.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+/**
+ * 스케줄러를 통해서 Job 실행
+ */
+@Component
+@RequiredArgsConstructor
+public class SampleScheduler {
+
+    private final Job testJob;
+    private final JobLauncher jobLauncher; // 스케줄링을 활용하여 Job 실행
+
+    @Scheduled(cron = "0 */1 * * * *") // 1분마다 실행할 수 있게 함
+    public void testJobRun() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+
+        JobParameters jobParameters = new JobParameters(
+                Collections.singletonMap("requestTime", new JobParameter(System.currentTimeMillis(), Long.class))
+        );
+
+        jobLauncher.run(testJob, jobParameters);
+    }
+}
